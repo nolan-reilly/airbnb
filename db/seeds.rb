000836c20799ac08a02917f58ab2ef493d33989f
Nolan Reilly <nolanreilly02@gmail.com>
2025-06-29 14:10:12 -0500

    Add Active Storage for images

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,6 @@
 
-20.times do
-  Property.create!({
+6.times do |i|
+  property = Property.create!({
     name: Faker::Name.name,
     description: Faker::Lorem.sentence(word_count: 15),
     headline: Faker::Lorem.sentence(word_count: 5),
@@ -11,4 +11,7 @@
     country: Faker::Address.country,
     price: Money.from_amount((50..100).to_a.sample, "USD")
   })
+
+  property.images.attach(io: File.open("db/images/property_#{i + 1}.jpg"), filename: property.name)
+  property.images.attach(io: File.open("db/images/property_#{i + 7}.jpg"), filename: property.name)
 end
